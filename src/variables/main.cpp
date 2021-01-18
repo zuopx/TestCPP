0121d28cb22f84c3da865c432f4a33944b7ad2c7
Percy Cho <percycho@outlook.com>
2021-01-18 22:24:08 +0800

    extern, struct

--- src/variables/main.cpp
+++ src/variables/main.cpp
@@ -7,20 +7,27 @@ using namespace std;
 /*
 声明局部变量，不会默认初始化？No，会默认初始化；
 */
-int a;
 void func1();
-
+int a;
 
 /*
 #define 也是声明常量的方式
 */
-#define LENGTH 10
-const int WIDTH = 5;
 void func2();
+#define LENGTH 10
+#define max(a, b) (a > b ? a : b) // 带参宏名
+const int WIDTH = 5;
+
+/*存储类修饰符
+static、extern 决定变量、函数的可见性范围和声明周期
+*/
+void func3();
+// extern int counter;  // counter必须声明在别的文件中，extern表示从别的文件找
 
 int main(int argc, char const *argv[])
 {
-    func2();
+    func3();
+    func3();
     return 0;
 }
 
@@ -35,4 +42,12 @@ void func1()
 void func2()
 {
     cout << LENGTH * WIDTH << endl;
+    cout << max(1, 2) << endl;
+}
+
+void func3()
+{
+    static int i = 0;  // 由于被static修饰了，变量i只会声明一次，而且存在直到程序退出
+    i++;
+    cout << i << endl;
 }
